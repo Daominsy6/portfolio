7698d04513c051e01c91322c78bd5f5b2fefd5b7
Daominsy6 <Daominsyyabut6@gmail.com>
2025-05-06 14:11:58 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: ["./index.html"], 
+  theme: {
+    extend: {
+      colors: {
+        primary: "#c00101",
+        secondary: "#1D1616"
+      }
+    },
+  },
+  plugins: [],
+}
+
